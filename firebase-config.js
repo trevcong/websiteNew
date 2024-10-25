058cad7112465d03c583bd54784870dff2026334
Trevor Conger <149925363+trevcong@users.noreply.github.com>
2024-10-25 02:19:29 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // website/firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyBOt-v2aqoop_B9iF8wkGq-T2lIPNkdk8k",
-    authDomain: "websitedb-a1827.firebaseapp.com",
-    databaseURL: "https://websitedb-a1827-default-rtdb.firebaseio.com",
-    projectId: "websitedb-a1827",
-    storageBucket: "websitedb-a1827.appspot.com",
-    messagingSenderId: "976408279985",
-    appId: "1:976408279985:web:d86e3bfce978fec9175b90",
-    measurementId: "G-ZHBCKS0JKY"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
